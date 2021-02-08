cdda04c5fcf60c7d5ef397654aad362b3b896968
Twinkal Parmar <twinkal@promactinfo.com>
2021-02-08 18:05:31 +0530

    Add required configuration for beanstalk deployment

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,7 +14,12 @@ namespace CICDExample
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
